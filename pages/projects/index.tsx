4bf24c483d9c6fe30e04daa7bca03969fb2005c7
muhananaufal <muhananaufal8@gmail.com>
2025-01-06 17:53:33 +0700

    fix projects index.tsx

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,22 +1,18 @@
-"use client";
-import { useEffect } from "react";
-import { Curve, Ready } from "@/components";
-import {
-	HeroProjects,
-	ListsProjects,
-	Publication,
-} from "@/container";
+'use client';
+import { useEffect } from 'react';
+import { Curve, Ready } from '@/components';
+import { HeroProjects, ListsProjects } from '@/container';
 
 export default function Presentation() {
 	useEffect(() => {
 		(async () => {
-			const LocomotiveScroll = (await import("locomotive-scroll")).default;
+			const LocomotiveScroll = (await import('locomotive-scroll')).default;
 			const locomotiveScroll = new LocomotiveScroll();
 		})();
 	}, []);
 	return (
 		<>
-			<Curve backgroundColor={"#DBE2EF"}>
+			<Curve backgroundColor={'#DBE2EF'}>
 				<HeroProjects />
 				<ListsProjects />
 				<Ready />
