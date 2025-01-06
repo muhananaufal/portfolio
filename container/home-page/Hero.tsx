ed99e4eb1f23615ee5018150dc747c10c3bb2ea7
muhananaufal <muhananaufal8@gmail.com>
2025-01-06 22:42:35 +0700

    disabled inspect and select

--- container/home-page/Hero.tsx
+++ container/home-page/Hero.tsx
@@ -31,6 +31,7 @@ export default function Hero() {
 											src={fireflyGif2}
 											alt="img"
 											className="w-auto h-[95px] lg:w-auto lg:h-auto md:w-[100px] md:h-[63px] sm:w-[74px] sm:h-[45px] xm:w-[64px] xm:h-[40px] object-cover xl:mt-[15px] mt-[10px] rounded-[10px]"
+											unoptimized
 										/>
 									</motion.span>
 									<h1 className="heading tracking-[-1.3px] text-[#212121] font-semibold font-FoundersGrotesk uppercase">Experiences</h1>
@@ -39,7 +40,7 @@ export default function Hero() {
 							</h1>
 						</div>
 						<div>
-							<Image src={naufalGif} alt="awwwards" width={60} height={60} className="w-fit h-fit" />
+							<Image src={naufalGif} alt="awwwards" width={60} height={60} className="w-fit h-fit" unoptimized/>
 						</div>
 					</div>
 				</div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,50 @@
 import '@/styles/globals.css';
 import { Footer, Navbar } from '@/components';
 import { AnimatePresence } from 'framer-motion';
-import { Toaster } from 'sonner';
+import { Toaster, toast } from 'sonner';
 import { useEffect } from 'react';
 
 export default function App({ Component, pageProps, router }: { Component: any; pageProps: any; router: any }) {
 	useEffect(() => {
+		// Disable right-click context menu
 		const disableContextMenu = (event: MouseEvent) => {
 			event.preventDefault();
-			console.log('Right-click is disabled globally.');
+		};
+
+		// Disable keyboard shortcuts for DevTools
+		const disableDevToolsShortcuts = (event: KeyboardEvent) => {
+			if (
+				(event.ctrlKey && event.shiftKey && ['I', 'J', 'C', 'U'].includes(event.key.toUpperCase())) || // Ctrl+Shift+I/J/C/U
+				event.key === 'F12' // F12
+			) {
+				event.preventDefault();
+			}
+		};
+
+		// Disable text/image dragging
+		const disableDrag = (event: DragEvent) => {
+			event.preventDefault();
 		};
 
 		document.addEventListener('contextmenu', disableContextMenu);
+		document.addEventListener('keydown', disableDevToolsShortcuts);
+		document.addEventListener('dragstart', disableDrag);
 
 		return () => {
 			document.removeEventListener('contextmenu', disableContextMenu);
+			document.removeEventListener('keydown', disableDevToolsShortcuts);
+			document.removeEventListener('dragstart', disableDrag);
 		};
 	}, []);
 
 	return (
-		<>
+		<div className="select-none">
 			<Toaster richColors closeButton expand={true} />
 			<Navbar />
 			<AnimatePresence mode="wait">
 				<Component key={router.route} {...pageProps} />
 			</AnimatePresence>
 			<Footer />
-		</>
+		</div>
 	);
 }
