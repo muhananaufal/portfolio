26d9b1017f268fdee4885475233a3c6a675ce4d6
muhananaufal <muhananaufal8@gmail.com>
2025-01-06 17:48:11 +0700

    fixing marquee file

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -3,7 +3,7 @@ import { TextMarquee } from '@/animation';
 
 export default function Marquee({ title, className, direction = 'right' }: TMarqueeProps & { direction?: 'left' | 'right' }) {
 	return (
-		<TextMarquee baseVelocity="0.8" direction={direction}>
+		<TextMarquee baseVelocity={0.8} direction={direction}>
 			<h1 className={`font-FoundersGrotesk bg-marquee font-normal border-y border-[#ffffff55] uppercase text-white  whitespace-nowrap tracking-[-5px] ${className}`}>{title} &nbsp;</h1>
 			<h1 className={`font-FoundersGrotesk bg-marquee font-normal border-y border-[#ffffff55] uppercase text-white  whitespace-nowrap tracking-[-5px] ${className}`}>{title} &nbsp;</h1>
 		</TextMarquee>
