0878cbd5faa70a2792d787d059f8c49c01b6fe6f
muhananaufal <muhananaufal8@gmail.com>
2025-01-08 10:38:36 +0700

    initial commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,9 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<title>Muhana Naufal Al-Atsari</title>
-			<Head />
+			<Head>
+				<meta name="google-site-verification" content="m2sljs2hmb6YARyu67zlv2RU0yFhMcVDsQg4Y-nX5xo" />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
