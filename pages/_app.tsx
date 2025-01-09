070b8213aee3221b479609b178109d43ab53bf33
muhananaufal <muhananaufal8@gmail.com>
2025-01-09 14:33:26 +0700

    innitial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { Toaster } from 'sonner';
 import { useDisableInteractions } from '@/hooks/useDisableInteractions';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/next';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps, router }: { Component: any; pageProps: any; router: any }) {
 	useDisableInteractions();
@@ -14,6 +15,10 @@ export default function App({ Component, pageProps, router }: { Component: any; 
 
 	return (
 		<div className="select-none">
+			<Head>
+				<title>Muhana Naufal Al-Atsari</title>
+				<meta name="google-site-verification" content="m2sljs2hmb6YARyu67zlv2RU0yFhMcVDsQg4Y-nX5xo" />
+			</Head>
 			<Toaster richColors closeButton expand={true} />
 			{shouldShowLayout && <Navbar />}
 			<AnimatePresence mode="wait">
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,6 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang="en">
-			<title>Muhana Naufal Al-Atsari</title>
 			<Head>
 				<meta name="google-site-verification" content="m2sljs2hmb6YARyu67zlv2RU0yFhMcVDsQg4Y-nX5xo" />
 			</Head>
