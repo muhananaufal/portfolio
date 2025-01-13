cf17cdb6b145acab73dfba9d2c140e4dd92be6e6
muhananaufal <muhananaufal8@gmail.com>
2025-01-13 16:58:00 +0700

    disable interaction

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ import Head from 'next/head';
 import { Chat } from '@/components/Chat';
 
 export default function App({ Component, pageProps, router }: { Component: any; pageProps: any; router: any }) {
-	// useDisableInteractions();
+	useDisableInteractions();
 
 	const routesWithoutNavbarAndFooter = ['/articles', '/rewinds/2023', '/rewinds/2024', '/rewinds/2025', '/me'];
 	const shouldShowLayout = !routesWithoutNavbarAndFooter.includes(router.route);
